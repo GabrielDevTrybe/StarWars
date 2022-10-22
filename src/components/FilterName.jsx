8721a034ee5921a9dc3a56d8a67d0c83bb59971c
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-10-22 07:57:31 -0300

    requisito 02 feito

---
+++ src/components/FilterName.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+// // import MyContext from '../context/MyContext';
+
+// function FilterName() {
+//   return (
+
+//     <label htmlFor="name">
+//       Name:
+//       <input
+//         type="text"
+//         name="name"
+//       />
+//     </label>
+//   );
+// }
+
+// export default FilterName;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,51 +1,71 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import MyContext from '../context/MyContext';
 
 function Table() {
   const contexto = useContext(MyContext);
-  console.log(contexto);
+  const [nameFilter, setName] = useState('');
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setName(value);
+  };
+
   return (
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
+    <>
+      <label htmlFor="name">
+        Name:
+        <input
+          data-testid="name-filter"
+          type="text"
+          name="name"
+          onChange={ handleChange }
+          value={ nameFilter }
+        />
+      </label>
 
-      <tbody>
-
-        { contexto.length
-        && contexto.map((e, index) => (
-          <tr key={ index + 1 }>
-            <td>{e.name}</td>
-            <td>{e.rotation_period}</td>
-            <td>{e.orbital_period}</td>
-            <td>{e.diameter}</td>
-            <td>{e.climate}</td>
-            <td>{e.gravity}</td>
-            <td>{e.terrain}</td>
-            <td>{e.surface_water}</td>
-            <td>{e.population}</td>
-            <td>{e.films}</td>
-            <td>{e.created}</td>
-            <td>{e.edited}</td>
-            <td>{e.url}</td>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+
+          {
+            contexto.filter((el) => el.name.includes(nameFilter)).map((e, index) => (
+              <tr key={ index }>
+                <td>{e.name}</td>
+                <td>{e.rotation_period}</td>
+                <td>{e.orbital_period}</td>
+                <td>{e.diameter}</td>
+                <td>{e.climate}</td>
+                <td>{e.gravity}</td>
+                <td>{e.terrain}</td>
+                <td>{e.surface_water}</td>
+                <td>{e.population}</td>
+                <td>{e.films}</td>
+                <td>{e.created}</td>
+                <td>{e.edited}</td>
+                <td>{e.url}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
   );
 }
 
